6325db38dd8487c1f3d52487614e8906aa35f5e0
Nandish Rao BR <132383207+Nandish-Rao-BR@users.noreply.github.com>
2023-06-27 19:16:30 +0530

    Add files via upload

---
+++ counterApp/Counterbycbf.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import Styles from "./counterApp.module.css"
+class Counterbycbf extends Component {
+    constructor() {
+        super()
+        this.state ={
+            count:0
+        }
+    }
+    handleIncrement =()=> {
+        this.setState({count:this.state.count+1})
+    }
+    handleDecrement = ()=>{
+        this.setState({count:this.state.count-1})
+ 
+    }
+    handleReset =()=>{
+        this.setState({count:0})
+    }
+  render() {
+    return (
+        
+        <div className='main-div' >
+            <div className={ Styles.container}>
+            <h1>Counter App using Class based Component</h1>
+            <h1> Count :  {this.state.count} </h1>
+            <button onClick={this.handleIncrement}>Increment</button>
+            <button onClick={this.handleDecrement}>Decrement</button>
+            <button onClick={this.handleReset}>Reset</button>
+      
+        </div>
+        </div>
+       
+    )
+  }
+}
+export default Counterbycbf
---
+++ counterApp/Counterbyfbf.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import Style from "./counterApp.module.css"
+const Counterbyfbf = () => {
+    let [count,setCount]=useState(0)
+    let handleIncrement =( )=>{
+        setCount(count+1)
+    }
+    let handleDecrement = ()=> {
+        setCount(count-1)
+    }
+    let handleReset = () =>{
+        setCount(0)
+    }
+  return (
+    
+    <div className='main-div' >
+            <div className={ Style.container}>
+            <h1>Counter App using Function based Component</h1>
+            <h1> Count :  {count} </h1>
+            <button onClick={handleIncrement}>Increment</button>
+            <button onClick={handleDecrement}>Decrement</button>
+            <button onClick={handleReset}>Reset</button>
+      
+        </div>
+        </div>
+  )
+}
+
+export default Counterbyfbf
